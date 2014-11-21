06859083b85a6f4cb42633bf2991c2d8f072f28a
Lennart Regebro <lregebro@redhat.com>
2014-11-21 16:42:14 +0100

    Get the Satellite connection parameters from Heat
    Change-Id: I1a66d2388f72b718169404232eac4861199f457c

--- tuskar_sat_ui/nodes/tabs.py
+++ tuskar_sat_ui/nodes/tabs.py
@@ -11,15 +11,28 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import horizon.messages
+import json
+import logging
 import requests
 import urllib
 
 from collections import namedtuple
+from django.conf import settings
 from horizon import tabs
+from requests_oauthlib import OAuth1
+from six import string_types
+from tuskar_ui import api
 from tuskar_ui.infrastructure.nodes import tabs as nodes_tabs
+
 from .tables import ErrataTable
 
+SAT_CONF_KEYS = ['compute-1::ExtraConfig', 'Satellite']
+SAT_HOST_PARAM = 'SatelliteHost'
+SAT_AUTH_PARAM = 'SatelliteAuth'
+SAT_ORG_PARAM = 'SatelliteOrg'
 
+LOG = logging.getLogger('tuskar_sat_ui')
 ErrataItem = namedtuple('ErrataItem', ['title', 'type', 'id', 'issued'])
 
 
@@ -31,13 +44,54 @@ class DetailOverviewTab(nodes_tabs.DetailOverviewTab):
         if result['node'].uuid is None:
             return result
 
-        # Some currently hardcoded values:
-        mac = '"52:54:00:4F:D8:65"'  # Hardcode for now
-        host = 'http://sat-perf-04.idm.lab.bos.redhat.com'  # Hardcode for now
-        auth = ('admin', 'changeme')
+        # Find the stack
+        try:
+            plan = api.tuskar.Plan.get_the_plan(request)
+            stack = api.heat.Stack.get_by_plan(self.request, plan)
+        except Exception as e:
+            LOG.exception(e)
+            horizon.messages.error(request,
+                                   _("Could not retrieve errata."))
+            return result
+
+        if stack is None:
+            return result
+
+        config = stack.parameters
+        # Find the Satellite configuration data
+
+        for key in SAT_CONF_KEYS:
+            if key in config:
+                config = config[key]
+            if isinstance(config, string_types):
+                # ExtraConfig stores it as json.
+                config = json.loads(config)
+
+        for param in [SAT_HOST_PARAM, SAT_AUTH_PARAM, SAT_ORG_PARAM]:
+            if param not in config:
+                horizon.messages.error(request,
+                                       _("No Satellite configuration found."))
+                return result
+
+        host = config[SAT_HOST_PARAM]
+        auth = config[SAT_AUTH_PARAM].split(':')
+        organization = config[SAT_ORG_PARAM]
+
+        if auth[0] == 'oauth':
+            auth = OAuth1(auth[1], auth[2])
+        elif auth[0] == 'basic':
+            auth = auth[1], auth[2]
+        else:
+            horizon.messages.error(
+                request,
+                _(('Satellite configuration error, '
+                  'unknown authentication "%s".') % auth[0]))
+
+        addresses = result['node'].addresses
 
         # Get the errata here
         host = host.strip('/')  # Get rid of any trailing slash in the host url
+        verify = not getattr(settings, 'OPENSTACK_SSL_NO_VERIFY', False)
 
         # Pick up the UUID from the MAC address This makes no sense, as we
         # need both MAC address and the interface, and we don't have the
@@ -45,22 +99,30 @@ class DetailOverviewTab(nodes_tabs.DetailOverviewTab):
         # Satellite UUID isn't the same as this one, and it probably isn't we
         # need to store a mapping somewhere.
         url = '{host}/katello/api/v2/systems'.format(host=host)
-        for interface in ['eth0', 'eth1', 'en0', 'en1']:
+        uuid = None
+        for mac in addresses:
+            for interface in ['eth0', 'eth1', 'en0', 'en1']:
 
-            q = 'facts.net.interface.{iface}.mac_address:{mac}'.format(
-                iface=interface, mac=mac)
-            r = requests.get(url, params={'search': q}, auth=auth)
-            results = r.json()['results']
-            if results:
-                break
-        else:
+                q = 'facts.net.interface.{iface}.mac_address:{mac}'.format(
+                    iface=interface, mac=mac)
+                params = {'search': q, 'organization_id': organization}
+                r = requests.get(url, params=params, auth=auth, verify=verify)
+                r.raise_for_status()  # Raise an error if the request failed
+                results = r.json()['results']
+                if results:
+                    uuid = results[0]['uuid']
+                    break
+
+        if uuid is None:
             # No node found
             result['errata'] = None
             return result
 
-        uuid = results[0]['uuid']
         errata_url = '{host}/katello/api/v2/systems/{id}/errata'
-        r = requests.get(errata_url.format(host=host, id=uuid), auth=auth)
+        r = requests.get(errata_url.format(host=host, id=uuid),
+                         auth=auth,
+                         verify=verify)
+        r.raise_for_status()  # Raise an error if the request failed
         errata = r.json()['results']
         if not errata:
             result['errata'] = None
