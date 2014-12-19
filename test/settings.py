0b0c1c99510c0a4df78861935866a2fab6aecdfe
Radomir Dopieralski <openstack@sheep.art.pl>
2014-12-19 15:20:54 +0100

    Silence the warnings about use of "with" in tests
    Change-Id: I67680159b75b17faf36c07b6a643a6dcf2b2c8b0

--- test/settings.py
+++ test/settings.py
@@ -1,3 +1,5 @@
+import warnings
+
 import openstack_dashboard.enabled
 from openstack_dashboard.utils import settings
 import test.enabled
@@ -7,3 +9,8 @@ from tuskar_ui.test.settings import *  # noqa
 INSTALLED_APPS = list(INSTALLED_APPS)  # Make sure it's mutable
 settings.update_dashboards([openstack_dashboard.enabled, test.enabled],
                            HORIZON_CONFIG, INSTALLED_APPS)
+
+# Silences the warning about with statements.
+warnings.filterwarnings('ignore', 'With-statements now directly support '
+                        'multiple context managers', DeprecationWarning,
+                        r'^tuskar_ui[.].*[._]tests$')
