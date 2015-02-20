db30177dce34a5ed24c306228e751f7eb4d4b6d5
Radomir Dopieralski <openstack@sheep.art.pl>
2015-02-20 18:09:06 +0100

    Render roles without images differently
    Change-Id: I8b595bde1dc242094614b64af104e3e4d7d4c0d8

--- tuskar_boxes/overview/views.py
+++ tuskar_boxes/overview/views.py
@@ -148,6 +148,12 @@ class IndexView(views.IndexView):
                     if field:
                         field.initial = 0
                     free_roles.append(role)
+                # For now, role is valid if it has image.
+                if role['role'].image:
+                    role['is_valid'] = True
+                else:
+                    pass
+                role['is_valid'] = False
             data['free_roles'] = free_roles
             data['flavors'] = list(
                 _flavor_data(self.request, flavors, flavor_roles))
